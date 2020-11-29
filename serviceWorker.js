d876028634218b5523383fd04130f5d9afe90b34
Kristian Tanuwijaya <tanuwijaya.kristian@gmail.com>
2020-11-29 13:40:28 +0700

    initial commit

--- serviceWorker.js
+++ serviceWorker.js
@@ -44,10 +44,33 @@ self.addEventListener('fetch', (event) => {
     console.info('Event: Fetch');
     var request = event.request;
     event.respondWith(
-        // Add caching strategy here
-        // e.g. Cache first
+        // IMPORTANT! CACHING STRATEGY HERE: https://blog.bitsrc.io/5-service-worker-caching-strategies-for-your-next-pwa-app-58539f156f52
+        // untuk aplikasi-aplikasi Ubaya yang berbasis PHP, sebaiknya menggunakan network first
+
+        //* // network first: try to fetch response from network. if succeeds, cache the response and return it. ff network fails, it falls back to cache
+        fetch(event.request).catch(function() {
+            return caches.match(event.request)
+        })
+        // */
+
+        /* // network only: solely uses the network to fetch and serve the response. does not fallback to any cache
+        fetch(event.request).then(function(networkResponse) {
+            return networkResponse
+        }) // */
+
+        /* // cache only: responds from the cache only. does not fall back to network
+        caches.open(cacheName).then(function(cache) {
+            cache.match(event.request).then(function(cacheResponse) {
+                return cacheResponse;
+            })
+        })
+        // */
+
+        /* // cache first: look for a response in the cache first, if response is found previously cached, then serve the cache. if not found it will fetch the response from network, serve it, and cache it for next time.
         caches.match(request).then((response) => {
-            if (response) { return response; }
+            if (response) {
+                return response;
+            }
             return fetch(request).then((response) => {
                 var responseToCache = response.clone();
                 caches.open(cacheName).then((cache) => {
@@ -57,7 +80,18 @@ self.addEventListener('fetch', (event) => {
                 });
                 return response;
             });
-        })
+        }) // */
+
+        /* //
+        caches.open(cacheName).then(function(cache) {
+            cache.match(event.request).then( function(cacheResponse) {
+                fetch(event.request).then(function(networkResponse) {
+                    cache.put(event.request, networkResponse)
+                })
+                return cacheResponse || networkResponse;
+            })
+        }) 
+        // */
     );
 });
 
