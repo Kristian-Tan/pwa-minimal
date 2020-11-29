baa2081040a2373e66f8db58d037a5eed4dedb8c
Kristian Tanuwijaya <tanuwijaya.kristian@gmail.com>
2020-11-29 13:16:22 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Minimal PWA
+
+## About
+- minimal (least amount of code) to make a web application qualify as installable PWA
+- no frills (css, js, library, framework), just plain old html and javascript (also some image/png/jpg for mandatory icon)
+- the goal is to show the bare minimum to be installable by chrome/chromium (specification might be changed later, so it should work around last commit date)
+- because it's minimal, it WILL violate many best practice for PWA
+
+## Credits
+- web.dev
+- github.com/gauravbehere/pwa-starter-demo
+
---
+++ serviceWorker.js
@@ -0,0 +1,90 @@
+var cacheName = 'cache-v3';
+
+/* 
+* Files to be served from cache
+*/
+var files = [
+    './',
+    './index.html',
+    './app.js',
+    './manifest.json',
+    './images/icon_192.png',
+    './images/icon_512.png',
+];
+
+
+self.addEventListener('install', (event) => {
+    console.info('Installing Service Worker');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then((cache) => {
+                return cache.addAll(files)
+                    .then(() => {
+                        console.info('Sucessfully Cached');
+                        return self.skipWaiting();
+                    })
+                    .catch((error) => {
+                        console.error('Failed to cache', error);
+                    })
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    console.info('Activating service worker');
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== cacheName) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        }).then(function () {
+            return self.clients.claim();
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    console.info('Event: Fetch');
+    var request = event.request;
+    event.respondWith(
+        /**
+         * Add caching strategy here
+         * e.g. Cache first
+         */
+        caches.match(request).then((response) => {
+            if (response) {
+                return response;
+            }
+            return fetch(request).then((response) => {
+                var responseToCache = response.clone();
+                caches.open(cacheName).then((cache) => {
+                    cache.put(request, responseToCache).catch((err) => {
+                        console.warn(request.url + ': ' + err.message);
+                    });
+                });
+                return response;
+            });
+        })
+    );
+});
+
+self.addEventListener('push', (event) => {
+    console.info('Event: Push', event);
+    event.waitUntil(self.registration.showNotification("test notification", {body: event.body}));
+});
+
+
+self.addEventListener('sync', function(event) {
+    console.info('Event: Sync', event);
+    /**
+     * Add logic to send requests to backend when sync happens
+     */
+    self.registration.showNotification("Syncing Now");
+  });
+
+
+  
